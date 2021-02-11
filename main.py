93e6cbe8aee6d7ebe0a67bef716d23fdcd5d4e80
Kimjinhyeon0 <rlawlsgus117@gmail.com>
2021-02-12 02:01:49 +0900

    third commit

--- main.py
+++ main.py
@@ -1,55 +1,28 @@
 import sys
+
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import QCoreApplication
+from PyQt5 import uic
+
+form_class = uic.loadUiType("untitled_ui.ui")[0]
 
 
-class MyApp(QWidget):
+class MyWindow(QMainWindow, form_class):
 
     def __init__(self):
         super().__init__()
-        self.initUI()
+        self.setupUi(self)
+        self.addbutton.clicked.connect(self.add_clicked)
 
-    def initUI(self):
-        self.AddButton = QPushButton("File Open")
-        self.AddButton.clicked.connect(self.AddButtonClicked)
-        self.label = QLabel()
+    def add_clicked(self):
+        file_names = QFileDialog.getOpenFileNames(self)
 
-        ConvertButton = QPushButton('Convert')
-
-        width_layout = QVBoxLayout()
-        width_layout.addStretch(20)
-        width_layout.addWidget(self.AddButton)
-        width_layout.addStretch(20)
-        width_layout.addWidget(ConvertButton)
-        width_layout.addStretch(1)
-
-        height_layout = QHBoxLayout()
-        height_layout.addWidget(self.label)
-        height_layout.addStretch(50)
-        height_layout.addLayout(width_layout)
-        height_layout.addStretch(1)
-
-        self.setLayout(height_layout)
+        for file in file_names[0]:
+            exist = self.textEdit.toPlainText()
+            self.textEdit.setText(exist + file + '\n')
 
 
-        self.setWindowTitle('쎈놈')
-        self.setWindowIcon(QIcon('web.png'))
-        self.resize(500, 200)
-        self.center()
-        self.show()
-
-    def AddButtonClicked(self):
-        fname = QFileDialog.getOpenFileName(self)
-        self.label.setText(fname[0])
-
-    def center(self):
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
-
-if __name__ == '__main__':
-   app = QApplication(sys.argv)
-   ex = MyApp()
-   sys.exit(app.exec_())
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWindow = MyWindow()
+    myWindow.show()
+    app.exec_()
